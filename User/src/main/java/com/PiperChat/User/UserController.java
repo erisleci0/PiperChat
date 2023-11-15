ecd983eea6c3265cffb3898b7e71510efe2f2acf
erisleci0 <lecieris@hotmail.com>
2023-11-15 22:40:03 +0100

    Add endpoint to retrieve all users

--- User/src/main/java/com/PiperChat/User/UserController.java
+++ User/src/main/java/com/PiperChat/User/UserController.java
@@ -1,7 +1,13 @@
 package com.PiperChat.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -10,5 +16,11 @@ public class UserController {
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @GetMapping(path = "/users")
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
     }
 }
