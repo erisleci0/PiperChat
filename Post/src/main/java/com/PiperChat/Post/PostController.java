ede4b9ab469a3b6d8ada779f13c180d3d7c22b9c
erisleci0 <lecieris@hotmail.com>
2024-01-30 14:03:23 +0100

    Add PostController

---
+++ Post/src/main/java/com/PiperChat/Post/PostController.java
@@ -0,0 +1,22 @@
+package com.PiperChat.Post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PostController {
+    private PostService postService;
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping(path = "/posts")
+    public ResponseEntity<List<PostEntity>> findAll(){
+        return ResponseEntity.ok(postService.findAllUsers());
+    }
+}
